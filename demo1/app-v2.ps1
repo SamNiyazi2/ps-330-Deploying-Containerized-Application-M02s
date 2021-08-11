2b3d467719e9603663dd1283ccab22fa8aa11c20
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-08-11 13:00:49 -0500

    Completed M02-02.

--- demo1/app-v2.ps1
+++ demo1/app-v2.ps1
@@ -1,29 +1,29 @@
 
 # create app network:
-docker network create wired-brain
+docker network create ps-330-wired-brain
 
 # database:
-docker run -d --network wired-brain `
+docker run -d --network ps-330-wired-brain `
  --name products-db-test `
  -e POSTGRES_PASSWORD=wiredtest `
  psdockerrun/products-db
 
 # products API:
-docker run -d --network wired-brain `
+docker run -d --network ps-330-wired-brain `
  --name products-api-test `
  -p 8081:80 `
  -v "$(pwd)/config/products-api:/app/config" `
  psdockerrun/products-api
 
 # stock API:
-docker run -d --network wired-brain `
+docker run -d --network ps-330-wired-brain `
  --name stock-api-test `
  -p 8082:8080 `
  -e POSTGRES_CONNECTION_STRING="host=products-db-test port=5432 user=postgres password=wiredtest dbname=postgres sslmode=disable" `
  psdockerrun/stock-api
 
 # website
-docker run -d --network wired-brain `
+docker run -d --network ps-330-wired-brain `
  --name web-test `
  -p 8080:80 `
  -e Environment=TEST `
